b1d29c9d6cfe99a06c23a9e68339fd2e71e22ddf
Dephoh <dfoley3838@gmail.com>
2023-03-13 18:50:33 -0400

    Third Meeting

--- client/client.js
+++ client/client.js
@@ -1,27 +1,66 @@
-const form = document.querySelector('form');
-const loadingElement = document.querySelector('.loading')
-const API_URL = 'http://locahost:8000/tweet'
-loadingElement.style.display = 'none';
+const form = document.querySelector("form");
+const loadingElement = document.querySelector(".loading");
+const tweetsElement = document.querySelector(".tweets");
+const API_URL = "http://localhost:8000/tweet";
+
+const listAllTweets = () => {
+    tweetsElement.innerHTML = '';
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((allTweets) => {
+      console.log(allTweets);
+      allTweets.reverse();
+      allTweets.forEach((tweet) => {
+        const div = document.createElement("div");
+        const header = document.createElement("h3");
+        header.textContent = tweet.name;
+
+        const content = document.createElement("p");
+        content.textContent = tweet.content;
+
+        const date = document.createElement("small");
+        date.textContent = new Date(tweet.created);
+
+        div.appendChild(header);
+        div.appendChild(content);
+        div.appendChild(date);
+
+        tweetsElement.appendChild(div);
+        })
+    });
+};
+
+loadingElement.style.display = "none";
+
+listAllTweets();
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const name = formData.get('name');
-    const content = formData.get('content');
+  event.preventDefault();
+  const formData = new FormData(form);
+  const name = formData.get("name");
+  const content = formData.get("content");
 
-    const tweet = {
-        name,
-        content
-    };
-    console.log(tweet);
-    form.style.display = 'none';
-    loadingElement.style.display ='';
+  const tweet = {
+    name,
+    content,
+  };
+  console.log(tweet);
+  form.style.display = "none";
+  loadingElement.style.display = "";
 
-    fetch(API_URL, {
-        method: 'POST',
-        body: JSON.stringify(tweet),
-        headers: {
-            'content-type': 'application/json'
-        }
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify(tweet),
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((createdTweet) => {
+      console.log(createdTweet);
+      form.reset();
+      form.style.display = "";
+      loadingElement.style.display = "none";
+      listAllTweets();
     });
-})
+});
--- server/index.js
+++ server/index.js
@@ -1,20 +1,64 @@
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
+var morgan = require("morgan");
+var monk = require("monk");
+var Filter = require("bad-words");
+
 var app = express();
 
+var db = monk("127.0.0.1/twitter");
+filter = new Filter();
+
+var tweets = db.get("tweets");
+
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res) => {
-    res.json({
-        message: 'Hi'
-    })
-})
+const isValidTweet = (tweet) => {
+  if (
+    tweet.name &&
+    tweet.name.toString().trim() !== "" &&
+    tweet.content &&
+    tweet.content.toString().trim() !== ""
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-app.post('/tweet',(req,res) => {
-    console.log(req.body)
-})
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hi",
+  });
+});
+
+app.get("/tweet", (req, res) => {
+  tweets.find().then((allTweets) => {
+    res.json(allTweets);
+  });
+});
+
+app.post("/tweet", (req, res) => {
+  if (isValidTweet(req.body)) {
+    const tweet = {
+      name: filter.clean(req.body.name.toString()),
+      content: filter.clean(req.body.content.toString()),
+      created: new Date(),
+    };
+
+    tweets.insert(tweet).then((createdTweet) => {
+      res.json(createdTweet);
+    });
+  } else {
+    res.status(422);
+    res.json({
+      message: "Name and Content are required",
+    });
+  }
+});
 
 app.listen(8000, () => {
-    console.log('Listening on port 8000 ...')
+  console.log("Listening on port 8000 .......");
 });
